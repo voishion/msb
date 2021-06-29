b0b40bc331082375df52f1eeaf9233821a0fd858
李露 <lilu@meishubao.com>
2021-06-29 16:57:31 +0800

    加入jenkins部署脚本1

--- yyb/jenkins/deploy_java.sh
+++ yyb/jenkins/deploy_java.sh
@@ -5,25 +5,25 @@ WEB_SERVER="10.0.0.7 10.0.0.8"
 #jenkins打包目录
 SOURCE_DIR=/opt
 #服务器部署目录
-TARGET_DIR=/code
+TARGET_DIR=/soft/tomcat/webapps
 #文件名
 NAME=${DATE}-${git_version}
 
 #1.进入项目目录，将内容进行打包
 get_code(){
-  cd ${WORKSPACE} && \
-  tar czf ${SOURCE_DIR}/web-${NAME}.tar.gz ./*
+  cd ${WORKSPACE}
 }
 
 #2.将内容通过scp拷贝至web集群组
 scp_web_server(){
   for host in $WEB_SERVER ;
   do
-    scp ${SOURCE_DIR}/web-${NAME}.tar.gz root@${host}:${SOURCE_DIR}
-    ssh root@${host} "mkdir -p ${TARGET_DIR}/web-${NAME} && \
-                      tar xf ${SOURCE_DIR}/web-${NAME}.tar.gz -C ${TARGET_DIR}/web-${NAME} && \
-                      rm -rf ${TARGET_DIR}/web && \
-                      ln -s ${TARGET_DIR}/web-${NAME} ${TARGET_DIR}/web"
+    scp target/*.war root@${host}:${SOURCE_DIR}/ROOT-${NAME}.war
+    ssh root@${host} "mkdir -p ${TARGET_DIR}/ROOT-${NAME} && \
+                      unzip ${SOURCE_DIR}/ROOT-${NAME}.war -d ${TARGET_DIR}/ROOT-${NAME} && \
+                      rm -rf ${TARGET_DIR}/ROOT && \
+                      ln -s ${TARGET_DIR}/ROOT-${NAME} ${TARGET_DIR}/ROOT && \
+                      /soft/tomcat/bin/shutdown.sh && /soft/tomcat/bin/startup.sh"
   done
 }
 
@@ -37,9 +37,10 @@ deploy(){
 rollback(){
   for host in $WEB_SERVER ;
   do
-    rollback_file=$(ssh root@${host} "find ${TARGET_DIR}/ -maxdepth 1 -type d -name "web-*-${git_version}"")
-    ssh root@${host} "rm -rf ${TARGET_DIR}/web && \
-                      ln -s ${rollback_file} ${TARGET_DIR}/web"
+    rollback_file=$(ssh root@${host} "find ${TARGET_DIR}/ -maxdepth 1 -type d -name "ROOT-*-${git_version}"")
+    ssh root@${host} "rm -rf ${TARGET_DIR}/ROOT && \
+                      ln -s ${rollback_file} ${TARGET_DIR}/ROOT && \
+                      /soft/tomcat/bin/shutdown.sh && /soft/tomcat/bin/startup.sh"
   done
 }
 
