a247db178fc5dcd18126d67bdeb7195b4cc3a058
李露 <lilu@meishubao.com>
2021-10-27 10:45:22 +0800

    项目名称修改

---
+++ springboot-study-log4j2/src/main/java/com/meishubao/log4j2/SpringbootStudyLog4j2Application.java
@@ -0,0 +1,13 @@
+package com.meishubao.log4j2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyLog4j2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyLog4j2Application.class, args);
+    }
+
+}
---
+++ springboot-study-log4j2/src/test/java/com/meishubao/log4j2/SpringbootStudyLog4j2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meishubao.log4j2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyLog4j2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
