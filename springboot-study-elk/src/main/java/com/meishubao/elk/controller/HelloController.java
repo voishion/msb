482c51f3b2e8b0dd6e8292249ee3629336f0e8c8
李露 <lilu@meishubao.com>
2022-01-19 15:49:30 +0800

    接口优化

--- springboot-study-elk/src/main/java/com/meishubao/elk/controller/HelloController.java
+++ springboot-study-elk/src/main/java/com/meishubao/elk/controller/HelloController.java
@@ -2,7 +2,7 @@ package com.meishubao.elk.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
@@ -11,14 +11,10 @@ import java.time.Instant;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello/{name}")
-    public String hello(@PathVariable String name){
-        log.debug("name:{}, author:{}", name, "voishion");
-        log.trace("name:{}, author:{}", name, "voishion");
-        log.error("name:{}, author:{}", name, "voishion");
-        log.warn("name:{}, author:{}", name, "voishion");
-        log.info("name:{}, author:{}", name, "voishion");
-        return String.format("系统时间：%s，名称：%s", Instant.now(), name);
+    @GetMapping("/collect")
+    public String collect(@RequestParam String keyword) {
+        log.info("日志记录, keyword:{}", keyword);
+        return String.format("系统时间：%s，名称：%s", Instant.now(), keyword);
     }
 
 }
