c86892a4c903b076377f52705fb7a7839742b20e
李露 <lilu@meishubao.com>
2022-01-19 13:05:15 +0800

    ELK项目初始化

---
+++ springboot-study-elk/src/main/java/com/meishubao/elk/SpringbootStudyElkApplication.java
@@ -0,0 +1,13 @@
+package com.meishubao.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyElkApplication.class, args);
+    }
+
+}
---
+++ springboot-study-elk/src/test/java/com/meishubao/elk/SpringbootStudyElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meishubao.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
