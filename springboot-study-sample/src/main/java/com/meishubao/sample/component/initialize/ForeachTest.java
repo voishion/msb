0663b7a03954f92ec16b65177dd79ceb2376ad70
李露 <lilu@meishubao.com>
2022-06-07 10:10:13 +0800

    arrayList.stream().forEach实现中止循环并退出

--- springboot-study-sample/src/main/java/com/meishubao/sample/component/initialize/ForeachTest.java
+++ springboot-study-sample/src/main/java/com/meishubao/sample/component/initialize/ForeachTest.java
@@ -5,9 +5,9 @@ import com.google.common.collect.Lists;
 import java.util.List;
 
 /**
- * arrayList.stream().forEach无法实现中止循环并退出
- * 解决办法：（使用arrayList.stream().anyMatch这个函数来进行遍历即可）
- * anyMatch()里接收一个返回值为boolean类型的表达式，只要返回true就会终止循环，这样可以将业务逻辑写在返回判断结果前
+ * 解决办法1：使用arrayList.stream().forEach通过内层抛出运行时异常，外层捕获异常的方式实现中止循环并退出
+ * 解决办法2：使用arrayList.stream().anyMatch函数可实现中止循环并退出，anyMatch()里接收一个返回值为boolean类型的
+ *          表达式，只要返回true就会终止循环，这样可以将业务逻辑写在返回判断结果前
  *
  * @author lilu
  */
@@ -15,12 +15,18 @@ public class ForeachTest {
 
     public static void main(String[] args) {
         List<String> list = Lists.newArrayList("a", "b", "c", "d");
-        /*list.stream().forEach(x -> {
-            if ("c".equals(x)) {
-                // 中止退出
-            }
-            System.out.println(x);
-        });*/
+        try {
+            list.stream().forEach(x -> {
+                if ("c".equals(x)) {
+                    throw new RuntimeException("终端退出");
+                }
+                System.out.println(x);
+            });
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Go1");
+
         list.stream().anyMatch(x -> {
             if ("c".equals(x)) {
                 return true;
@@ -28,6 +34,7 @@ public class ForeachTest {
             System.out.println(x);
             return false;
         });
+        System.out.println("Go1");
     }
 
 }
