82db03cd0c5d37343be16a4223b2a33c3bed9f5c
李露 <lilu@meishubao.com>
2022-10-14 08:53:28 +0800

    joda-time

--- springboot-study-sample/src/main/java/com/meishubao/sample/Main.java
+++ springboot-study-sample/src/main/java/com/meishubao/sample/Main.java
@@ -1,6 +1,13 @@
 package com.meishubao.sample;
 
-import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.date.DatePattern;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Minutes;
+import org.joda.time.format.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * @author lilu
@@ -8,8 +15,21 @@ import cn.hutool.core.util.StrUtil;
 public class Main {
 
     public static void main(String[] args) {
-        String str = StrUtil.format("WW{}SS", "李露");
-        System.out.println(str);
+        //String str = StrUtil.format("WW{}SS", "李露");
+        //System.out.println(str);
+        DateTime start = DateTime.parse("2022-10-13 07:24:01", DateTimeFormat.forPattern(DatePattern.NORM_DATETIME_PATTERN)),
+                   end = DateTime.parse("2022-10-13 18:09:36", DateTimeFormat.forPattern(DatePattern.NORM_DATETIME_PATTERN));
+        int days = Days.daysBetween(start, end).getDays();
+        System.out.println(days);
+
+        int minutes = Minutes.minutesBetween(start, end).getMinutes();
+        double value = new BigDecimal(String.valueOf(minutes)).divide(new BigDecimal("60"), 2, RoundingMode.HALF_UP).doubleValue();
+        System.out.println(value);
+
+        start = DateTime.parse("2022-10-13 08:30:00", DateTimeFormat.forPattern(DatePattern.NORM_DATETIME_PATTERN));
+        minutes = Minutes.minutesBetween(start, end).getMinutes();
+        value = new BigDecimal(String.valueOf(minutes)).divide(new BigDecimal("60"), 2, RoundingMode.HALF_UP).doubleValue();
+        System.out.println(value);
     }
 
 }
