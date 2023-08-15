bf6ab994c1d27f5fec78fd06bb20edc8d702283e
李露 <lilu1@hydsoft.com>
2023-08-15 16:45:11 +0800

    screw

---
+++ springboot-study-screw/src/main/java/com/meishubao/screw/SpringbootStudyScrewApplication.java
@@ -0,0 +1,13 @@
+package com.meishubao.screw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyScrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyScrewApplication.class, args);
+    }
+
+}
---
+++ springboot-study-screw/src/test/java/com/meishubao/screw/SpringbootStudyScrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meishubao.screw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyScrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
